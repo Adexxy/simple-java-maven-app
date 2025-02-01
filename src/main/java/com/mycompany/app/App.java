73cc69b943be7407b7b284b4bea78ac37615f2f3
Owoade Taiwo <adexxy@live.com>
2025-02-01 21:56:17 +0100

    using spring boot for persistency instead of java jvm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,49 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
 public class App {
-
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
-
-    public String getMessage() {
-        return MESSAGE;
+        SpringApplication.run(App.class, args);
     }
 }
+
+@RestController
+@RequestMapping("/api")
+class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
+
+
+
+
+
+
+
+// package com.mycompany.app;
+
+// /**
+//  * Hello world!
+//  */
+// public class App {
+
+//     private static final String MESSAGE = "Hello World!";
+
+//     public App() {}
+
+//     public static void main(String[] args) {
+//         System.out.println(MESSAGE);
+//     }
+
+//     public String getMessage() {
+//         return MESSAGE;
+//     }
+// }
